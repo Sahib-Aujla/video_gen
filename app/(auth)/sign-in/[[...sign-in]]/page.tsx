0c22e73d87fe052b95edb8931972ada0cf8a1409
Sahib.Aujla <sahibpreet568@gmail.com>
2025-05-31 13:01:57 +0530

    created sign in page

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignIn } from "@clerk/nextjs";
+import React from "react";
+import Image from "next/image";
+const Signin = () => {
+  return (
+    <div className="flex flex-row h-screen w-full">
+      <div className="w-1/2 h-full relative"><Image src='/signin.jpg' alt='signin' fill={true} /></div>
+      <div className="w-1/2 h-full flex justify-center items-center">
+        <SignIn />
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
---
+++ app/providers.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
+const Provider = ({children}: {children: React.ReactNode}) => {
+  return (
+    <ClerkProvider>{children}</ClerkProvider>
+  )
+}
+
+export default Provider
